ed04bb393f2312e1ad01d292a404650f03cc6fb1
MesutKizilay <mesutkizilay3868@gmail.com>
2024-02-03 19:30:11 +0300

    .

--- app/[...blog]/page.jsx
+++ app/[...blog]/page.jsx
@@ -2,5 +2,5 @@ import React from "react";
 
 export default function Blog({ params }) {
     console.log(params);
-    return <div>Blog sayfası: {params.blog}</div>;
+    return <div>Blog sayfası:  {params.blog}</div>;
 }
