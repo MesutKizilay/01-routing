529e7745cea07d2926f76904adfdb9a4f800adcb
Mesut Kızılay <mesutkizilay3868@gmail.com>
2024-02-04 21:52:47 +0300

    Add project files.

--- 01-routing/app/page.jsx
+++ 01-routing/app/page.jsx
@@ -1,3 +1,24 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Hello world!</div>;
+  return <div>
+    <Link prefetch={false} href="/hakkimizda">Hakkimizda</Link>
+    <br></br>
+    <Link prefetch={false} href="docs">Post 1</Link>
+    <br></br>
+    <Link prefetch={false} href="docs/asd">Post 2</Link>
+    <br></br>
+    <Link
+      prefetch={false}
+      //replace={true}
+      className="test"
+      href={{
+        pathname: '/hakkimizda',
+        query: {
+          name: 'Next.js',
+          surname: '3'
+        }
+      }}>Yeni Sayfa
+    </Link>
+  </div>;
 }
